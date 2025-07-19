adf2ef715067a5e073527b4b3201e5fe42f607a1
Hemanth-07102004 <hemanth.nimmala.7@gmail.com>
2025-07-19 22:37:41 +0530

    First commit

---
+++ evaluation-test.js
@@ -0,0 +1,82 @@
+//1.
+let age=20;
+console.log(typeof(age));
+
+//2.
+let price="150";
+numprice=Number(price);
+console.log(numprice);
+console.log(typeof(numprice));
+
+//3.
+let num1=10;
+let num2=5;
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+
+//4.
+
+
+//5.
+let a = 20;
+let b ="5";
+let c = 2;
+b = Number(b);
+let result = a + b + c;
+console.log("Result:", result);
+
+//6.
+let score
+console.log(score);
+console.log(typeof(score));
+score=50;
+console.log(score);
+console.log(typeof(score));
+
+//7.
+var num=5;
+console.log(num);//output is 10
+var num=6; // reassignment and re declartion allowed in var
+console.log(num);//output is 6
+
+let h=8;
+console.log(h);
+h=9; //reassigning is allowed
+console.log(h);//output is 9
+// let h=10;//declaration is not allowed
+// console.log(h);//error coming due to 'h' has already been declared or redeclaration is not allowed
+
+const g=10;
+console.log(g);//10
+// g=11;
+// console.log(g);//error:reassignment is not allowed
+// const g=12;
+// console.log(g);//error:redeclaration is not allowed too.
+
+//8.
+let base=10;
+let height="5";
+hei=Number(height);
+area=0.5*base*hei;
+console.log(area);
+
+//9.
+let marks="85";
+mark=Number(marks);
+add=15
+console.log(mark+add);//100
+
+//10.
+let message="Hello, World!"
+console.log(typeof(message));//string
+message=42;
+console.log(message);//42
+console.log(typeof(message));//number
+
+//11.
+var text="123abc";
+Numb=Number(text);
+console.log(Numb);//NaN
+console.log(typeof(Numb));//number
